b913763389cb3741a8bf77f7c4aae49552e00dc9
smhrd <smhrd@DESKTOP-VF30AJ1>
2022-01-28 12:38:22 +0900

    20220128_대외활동데이터_최보람

--- ActivityPlan/src/main/java/Controller/ModifyService.java
+++ ActivityPlan/src/main/java/Controller/ModifyService.java
@@ -5,18 +5,46 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class ModifyService
- */
+//import org.apache.catalina.tribes.util.Arrays;
+import java.util.*;
+
+import Model.MemberDAO;
+import Model.MemberDTO;
+
 public class ModifyService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("[ModifyService]");
 
+		String pw = request.getParameter("newPw");
+		String nickname = request.getParameter("newNickname");
+		String[] majors = request.getParameterValues("newMajors");
+
+		String major_pick = Arrays.toString(majors);
+
+		HttpSession session = request.getSession();
+		MemberDTO info = (MemberDTO) session.getAttribute("info");
+
+		// 아이디를 기준으로 정보 업데이트
+		String id = info.getUser_id();
+
+		info = new MemberDTO(id, pw, nickname, major_pick);
+
+		MemberDAO dao = new MemberDAO();
+		int cnt = dao.modify(info);
+
+		if (cnt > 0) {
+			System.out.println("회원정보 수정 성공");
+
+			// 성공하면 session 갱신
+			session.setAttribute("info", info);
+		} else {
+			System.out.println("회원정보 수정 실패");
+		}
+		response.sendRedirect("login.html");
+	}
 }
